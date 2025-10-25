1e52f1099e0bf33c9ec4f59063abc8433045ea21
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-10-25 10:59:17 +0200

    Added script for checking messages.
    Needed to create persistent browser context because just saving storage
    state wasn't enough to retain WhatsApp login.

--- scripts/whatsapp/check-messages.js
+++ scripts/whatsapp/check-messages.js
@@ -1,7 +1,8 @@
 import { chromium } from 'playwright';
+import 'dotenv/config';
 
 async function main() {
-  const browser = await chromium.launchPersistentContext( 'user-data/whatsapp', { headless: false });
+  const browser = await chromium.launchPersistentContext( 'user-data/whatsapp', { headless: process.env.HEADLESS !== 'false' });
   const page = await browser.newPage();
 
   await page.goto('https://web.whatsapp.com');
