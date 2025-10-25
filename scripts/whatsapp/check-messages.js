bac890cc0e1380ac46a37c67daa66968c8091306
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-10-25 15:45:05 +0200

    Implemented reading of chats.

--- scripts/whatsapp/check-messages.js
+++ scripts/whatsapp/check-messages.js
@@ -1,13 +1,38 @@
 import { chromium } from 'playwright';
 import 'dotenv/config';
 
+async function readChat(page, chat) {
+  await chat.click();
+  await page.waitForTimeout(1000);
+
+  const title = await page.locator('#main').locator('header').locator('span').first().innerText();
+
+  const unreadMessages = page.locator('xpath=//*[@id="main"]//div[.//span[text()="Today"]]/following::div[@role="row"]');
+  for (const message of await unreadMessages.all()) {
+    const text = await message.locator('span').nth(4).innerText();
+    await fetch('https://ntfy.sh/' + process.env.WHATSAPP_TOPIC, {
+      method: 'POST',
+      body: title + ': ' + text,
+    })
+  }
+}
+
 async function main() {
   const browser = await chromium.launchPersistentContext( 'user-data/whatsapp', { headless: process.env.HEADLESS !== 'false' });
   const page = await browser.newPage();
 
   await page.goto('https://web.whatsapp.com');
+  await page.getByRole('tab', { name: 'Unread' }).click();
+  await page.waitForTimeout(500);
 
-  await page.pause();
+  const allUnreadChats = page.getByRole('row');
+  for (const chat of await allUnreadChats.all()) {
+
+    const chatMuted = await chat.locator('span').filter({ hasText: /^mute-notifications-refreshed$/ }).isVisible();
+    if (!chatMuted) {
+      await readChat(page, chat);
+    }
+  }
 
   await browser.close();
 }
